24df353666cdd0f160f6d3c6f970af2ec7d36969
1486073356@qq.com <1486073356@qq.com>
2020-04-08 17:22:24 +0800

    'update'

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import {Tabbar} from './Tabbar.js'
 window.onload = function () {
 
-  let tabbar = new Tabbar('.tabbar',{
+  let tabbar = new Tabbar('.UIcomments-tabbar',{
     tabbr_line:true,
     hover_line:true
   });
